ab52d3b53d1af438d971d6824dd6ad30ace10453
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2022-03-17 16:52:00 -0300

    atualização da rede socias e resolvido escolha cor

--- js/script.js
+++ js/script.js
@@ -9,22 +9,22 @@ menu.onclick = () => {
 let themeToggler = document.querySelector('.theme-toggler');
 let toggleBtn = document.querySelector('.toggle-btn');
 
-toggleBtn.onclick = () => {
-    themeToggler.classList.toggle('active');
-};
+toggleBtn.onclick = () =>{
+  themeToggler.classList.toggle('active');
+}
 
-window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-    themeToggler.classList.remove('active');
-};
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  themeToggler.classList.remove('active');
+}
 
-document.querySelectorAll('.theme-toggler .theme-btn').forEach(btn => {
-
-    btn.onclick = () => {
-        let color = btn.style.background;
-        document.querySelector(':root').style.setProperty('--main-color', color);
-    }
+document.querySelectorAll('.theme-btn').forEach(btn =>{
+  
+  btn.onclick = () =>{
+    let color = btn.style.background;
+    document.querySelector(':root').style.setProperty('--main-color', color);
+  }
 
 });
 
